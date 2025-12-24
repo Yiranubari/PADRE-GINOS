f87156816826f24f57ecef251beb4de6f1486d44
Yiranubari <yiranubari4@gmail.com>
2025-12-24 11:43:32 +0100

    refactor pizza component to accept props for dynamic rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-const pizza = () => {
+const pizza = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "The Best Pizza in Town"),
-    React.createElement(
-      "p",
-      {},
-      "Come and try our delicious pizzas made with fresh ingredients."
-    ),
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
   ]);
 };
 
@@ -13,11 +9,34 @@ const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Welcome to Padre Gino's!"),
     React.createElement("p", {}, "The best Italian food in town."),
-    React.createElement(pizza),
-    React.createElement(pizza),
-    React.createElement(pizza),
-    React.createElement(pizza),
-    React.createElement(pizza),
+    React.createElement(pizza, {
+      name: "Margherita",
+      description: "Classic delight with 100% real mozzarella cheese",
+    }),
+    React.createElement(pizza, {
+      name: "Pepperoni",
+      description:
+        "A classic American taste! Relish the delectable flavor of Pepperoni, topped with extra cheese",
+    }),
+    React.createElement(pizza, {
+      name: "Veggie Supreme",
+      description:
+        "A colorful mix of fresh veggies on a bed of mozzarella cheese",
+    }),
+    React.createElement(pizza, {
+      name: "BBQ Chicken",
+      description:
+        "Grilled chicken pieces smothered in tangy BBQ sauce, topped with onions and cheese",
+    }),
+
+    React.createElement(pizza, {
+      name: "Hawaiian",
+      description: "A tropical treat with ham and pineapple on a cheesy base",
+    }),
+    React.createElement(pizza, {
+      name: "Four Cheese",
+      description: "A rich blend of four cheeses on a crispy crust",
+    }),
   ]);
 };
 
