0e85f7a12f9dbec4721443777c155c271a85d052
Yiranubari <yiranubari4@gmail.com>
2025-12-25 13:26:47 +0100

    chore: add image prop to Pizza component instances in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
-      <Pizza name="Hawaiian" description="ham, pineapple, n stuff" />
-      <Pizza name="Americano" description="french fries, hot dogs, n stuff" />
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n stuff"
+        image={"/public/pizzas/.webp"}
+      />
+      <Pizza
+        name="Hawaiian"
+        description="ham, pineapple, n stuff"
+        image={"/public/pizzas/.webp"}
+      />
+      <Pizza
+        name="Americano"
+        description="french fries, hot dogs, n stuff"
+        image={"/public/pizzas/.webp"}
+      />
     </div>
   );
 };
