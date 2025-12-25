81d4eef19de8bf08d3cd6ec927610a1fa3a3cb7d
Yiranubari <yiranubari4@gmail.com>
2025-12-25 14:35:03 +0100

    chore: refactor Pizza component to include onClick event for logging name

--- Pizza.jsx
+++ Pizza.jsx
@@ -7,7 +7,12 @@
 
 const Pizza = (props) => {
   return (
-    <div className="pizza">
+    <div
+      className="pizza"
+      onClick={() => {
+        console.log(props.name);
+      }}
+    >
       <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
