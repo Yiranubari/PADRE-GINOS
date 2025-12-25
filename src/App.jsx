6da50c7c52197557bb53da0048bfb9d4f37ec66b
Yiranubari <yiranubari4@gmail.com>
2025-12-25 10:51:35 +0100

    chore: refactor App component to use JSX syntax for improved readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import { createRoot } from "react-dom/client";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Welcome to Padre Gino's!"),
-    React.createElement("p", {}, "The best Italian food in town."),
-    React.createElement(pizza, {
-      name: "Margherita",
-      description: "Classic delight with 100% real mozzarella cheese",
-    }),
-    React.createElement(pizza, {
-      name: "Pepperoni",
-      description:
-        "A classic American taste! Relish the delectable flavor of Pepperoni, topped with extra cheese",
-    }),
-    React.createElement(pizza, {
-      name: "Veggie Supreme",
-      description:
-        "A colorful mix of fresh veggies on a bed of mozzarella cheese",
-    }),
-    React.createElement(pizza, {
-      name: "BBQ Chicken",
-      description:
-        "Grilled chicken pieces smothered in tangy BBQ sauce, topped with onions and cheese",
-    }),
-
-    React.createElement(pizza, {
-      name: "Hawaiian",
-      description: "A tropical treat with ham and pineapple on a cheesy base",
-    }),
-    React.createElement(pizza, {
-      name: "Four Cheese",
-      description: "A rich blend of four cheeses on a crispy crust",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Padre Gino's - Order Now</h1>
+      <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
