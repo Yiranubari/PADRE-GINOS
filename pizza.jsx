e333e0e0476431c264fb5025a9631a90b751c24d
Yiranubari <yiranubari4@gmail.com>
2025-12-25 10:08:07 +0100

    fix: refactor pizza component to use JSX syntax

---
+++ pizza.jsx
@@ -0,0 +1,17 @@
+// const pizza = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("p", {}, props.description),
+//   ]);
+// };
+
+const pizza = (props) => {
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export default pizza;
