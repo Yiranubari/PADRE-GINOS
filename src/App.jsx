8db517054e799422dc3f64132746c9a9409a0cd0
Yiranubari <yiranubari4@gmail.com>
2025-12-25 13:27:39 +0100

    chore: update image paths for Pizza component instances in App

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,17 @@ const App = () => {
       <Pizza
         name="Pepperoni"
         description="pep, cheese, n stuff"
-        image={"/public/pizzas/.webp"}
+        image={"/public/pizzas/pepperoni.webp"}
       />
       <Pizza
         name="Hawaiian"
         description="ham, pineapple, n stuff"
-        image={"/public/pizzas/.webp"}
+        image={"/public/pizzas/hawaiian.webp"}
       />
       <Pizza
         name="Americano"
         description="french fries, hot dogs, n stuff"
-        image={"/public/pizzas/.webp"}
+        image={"/public/pizzas/big_meat.webp"}
       />
     </div>
   );
