7c775f457fb99d0ffd589812040e805a9b41d9c2
Yiranubari <yiranubari4@gmail.com>
2025-12-25 10:57:45 +0100

    chore: update Pizza descriptions for accuracy

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ const App = () => {
       {/* <x-my-webcomponent></x-my-webcomponent> */}
       <h1>Padre Gino's - Order Now</h1>
       <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
-      <Pizza name="Hawaiian" description="pep, cheese, n stuff" />
-      <Pizza name="Americano" description="pep, cheese, n stuff" />
+      <Pizza name="Hawaiian" description="ham, pineapple, n stuff" />
+      <Pizza name="Americano" description="french fries, hot dogs, n stuff" />
     </div>
   );
 };
